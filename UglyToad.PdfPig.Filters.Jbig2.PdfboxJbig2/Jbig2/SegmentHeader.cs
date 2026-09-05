dc91a82c3810526c2a4293ad8ee05d5a6cb392dc
BobLd <38405645+BobLd@users.noreply.github.com>
2026-09-05 15:54:35 +0100

    Improve allocation

--- UglyToad.PdfPig.Filters.Jbig2.PdfboxJbig2/Jbig2/SegmentHeader.cs
+++ UglyToad.PdfPig.Filters.Jbig2.PdfboxJbig2/Jbig2/SegmentHeader.cs
@@ -141,15 +141,12 @@ namespace UglyToad.PdfPig.Filters.Jbig2.PdfboxJbig2.Jbig2
         {
             int countOfRTS = (int)(subInputStr.ReadBits(3) & 0xf);
 
-            byte[] retainBit;
-
             if (countOfRTS <= 4)
             {
                 // Short format
-                retainBit = new byte[5];
                 for (int i = 0; i <= 4; i++)
                 {
-                    retainBit[i] = (byte)subInputStr.ReadBit();
+                    subInputStr.ReadBit();
                 }
             }
             else
@@ -157,12 +154,11 @@ namespace UglyToad.PdfPig.Filters.Jbig2.PdfboxJbig2.Jbig2
                 // Long format
                 countOfRTS = (int)(subInputStr.ReadBits(29) & 0xffffffff);
 
-                int arrayLength = countOfRTS + 8 >> 3;
-                retainBit = new byte[arrayLength <<= 3];
+                int retainBitCount = (countOfRTS + 8 >> 3) << 3;
 
-                for (int i = 0; i < arrayLength; i++)
+                for (int i = 0; i < retainBitCount; i++)
                 {
-                    retainBit[i] = (byte)subInputStr.ReadBit();
+                    subInputStr.ReadBit();
                 }
             }
             return countOfRTS;
